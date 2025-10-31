3798a5a329f5d0ee2530fccad505eaf41a28333a
Sa1man9 <salmanmohd781@gmail.com>
2025-11-01 00:53:03 +0530

    thread safe eager singleton

---
+++ Singleton Design Pattern/ThreadSafeEagerSingleton.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Singleton{
+    private:
+    static Singleton * instance;
+    Singleton(){
+        cout<<"Singleton Constructor called. New Object created."<<endl;
+    }
+    public:
+
+    static Singleton* getInstance(){
+        return instance;
+    }
+};
+
+Singleton* Singleton:: instance= new Singleton();
+
+int main(){
+    Singleton* s1=Singleton::getInstance();
+    Singleton* s2=Singleton:: getInstance();
+    
+    cout<<(s1==s2)<<endl;
+
+}
